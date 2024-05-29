51abe5863832208a297da38a01f05af08bd075b0
rohan deshpnde <rohandeshpande1632@gmail.com>
2024-05-29 09:26:26 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import  find_packages, setup     
+
+def get_requirements():
+    requirements_list = []
+    return  requirements_list
+
+
+setup(
+name ='SENSOR_FAULT_PREDICTION',
+version ="0.0.1",
+author ="Rohan",
+author_email ='rohandeshpande1632@gmail.com',
+packages = find_packages(),
+install_requires = get_requirements()
+
+)
