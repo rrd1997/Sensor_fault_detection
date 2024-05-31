9ef98f5041225018d86a0ff3a90faf9d24efea26
rohan deshpnde <rohandeshpande1632@gmail.com>
2024-05-31 10:25:15 +0530

    logger file added

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 import os
 from src.exception import SensorException
+from src.logger import logging
 
 def test_exception():
     try:
+        logging.info("Brother here 1 error --zero devision error -- will occured")
         a=1/0
     except Exception as e:
          raise SensorException("Something went wrong!", sys.exc_info())
